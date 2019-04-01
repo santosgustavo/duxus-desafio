5ba82d64dad57f05a4c028f7772e38d6d9d508e3
Gustavo Oliveira <gustavosantos510@yahoo.com.br>
2019-04-01 17:30:44 -0300

    View Login

--- src/main/java/com/duxus/desafio/prjduxusdesafio/controller/LoginController.java
+++ src/main/java/com/duxus/desafio/prjduxusdesafio/controller/LoginController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,6 +18,11 @@ public class LoginController {
 
 	@Autowired
 	LoginService service;
+	
+	@GetMapping
+	public String login() {
+		return "formLogin";
+	}
 	
 	@RequestMapping(value="/autenticar", method=RequestMethod.POST)
 	public ResponseEntity<?> login(@RequestHeader String usuario,@RequestHeader String senha){
--- src/main/java/com/duxus/desafio/prjduxusdesafio/model/Usuario.java
+++ src/main/java/com/duxus/desafio/prjduxusdesafio/model/Usuario.java
@@ -1,5 +1,7 @@
 package com.duxus.desafio.prjduxusdesafio.model;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -7,8 +9,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Usuario {
-
+public class Usuario implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
